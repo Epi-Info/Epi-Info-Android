e293bbc204c29bff591c5df164361724750ae45a
cdc-dpbrown <ita3@cdc.gov>
2023-08-21 16:37:19 -0400

    Changes to improve ease of build.

---
+++ AppCode/build/generated/source/buildConfig/debug/gov/cdc/epiinfo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gov.cdc.epiinfo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "gov.cdc.epiinfo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 154;
+  public static final String VERSION_NAME = "1.5.4";
+}
